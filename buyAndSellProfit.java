538c49895fab78b951bb76d68cfc250b5bc75b5e
Sanjeevani Gandhakshe <98202766+Sanjeevani018@users.noreply.github.com>
2023-09-23 18:33:46 +0530

    Update buyAndSellProfit.java

--- buyAndSellProfit.java
+++ buyAndSellProfit.java
@@ -1,14 +1,14 @@
 public class ArraysCC{
     public static int buyAndSellStock(int prices[]){
-        int buyPrice=Integer.MAX_VALUE;
-        int maxProfit=0;
+        int buyPrice=Integer.MAX_VALUE;//lowest buyPrice for stock
+        int maxProfit=0;//max profit 
         for(int i=0;i<prices.length;i++){
-             if(buyPrice<prices[i]){
+             if(buyPrice<prices[i]){  //case 1:- can gain profit and update  to maxProfit
                 int profit=prices[i]-buyPrice;
                 maxProfit=Math.max(maxProfit, profit);
 
         }else{
-            buyPrice=prices[i];
+            buyPrice=prices[i];   //case 2:-it will gain loss so better Buy the stock
         }
         
     }return maxProfit;
